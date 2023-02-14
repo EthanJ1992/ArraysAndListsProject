e0e5e5ed1ec12e34e423b7d5a6d476c8748a55e1
Ethan Jacobs <jacobethan1992@gmail.com>
2023-02-13 22:35:39 -0500

    Arrays and List exercise

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -17,9 +17,9 @@ namespace ArraysAndLists
              * Name one List "evens"
              * Name the other List "odds"
              */
-            var evens = new List <int>();
-            var odds  = new List <int>();
-            foreach ( var num in numbers )
+            var evens = new List<int>();
+            var odds = new List<int>();
+            foreach (var num in numbers)
             {
                 if (num % 2 == 0)
                 {
@@ -33,26 +33,50 @@ namespace ArraysAndLists
             }
 
             Console.WriteLine("this is my even list");
-            for(int i = 0; i< evens.Count; i++) 
+            for (int i = 0; i < evens.Count; i++)
             {
                 Console.WriteLine(evens[i]);
             }
 
             Console.WriteLine("this is my odds list");
-            foreach (var num in odds) { 
-            } 
+            foreach (var num in odds) {
+            }
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
              *  if a number is even or odd.
              * Then add those numbers to either the evens List
              * or the odds List
              */
+            foreach (var item in array)
+            {
+                if (item % 2 == 0)
+                {
+                    evens.Add(item);
+                }
+                else
+                {
+                    odds.Add(item);
+                }
+            }
 
             /* Now using foreach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+
+            for (int i -0; i < evens.Count; i++)
+        }
+            Console.WriteLine($"{odds[i]} is an even number");
+        {
+            Console.WriteLine( );
+            Console.WriteLine( );
+
+            for (int i = 0; i < odds.Count; i++)
+            {
+            Console.WriteLine($"{odds[i]} is an odd number");
+            }
         }
     }
 }
+
